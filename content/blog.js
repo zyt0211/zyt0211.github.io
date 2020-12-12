bb6a029b98c327910a780c436c93919505b22ea1
zyt0211 <67827239+zyt0211@users.noreply.github.com>
2020-12-12 18:18:22 +0800

    Update blog.js

--- content/blog.js
+++ content/blog.js
@@ -1,19 +1,19 @@
 const blog=[
     {
         id:1,
-        title:"这是第一篇博客",
+        title:"网络爬虫技术应用",
         href:"https://www.bilibili.com/",
         image:"https://images.unsplash.com/photo-1533038590840-1cde6e668a91?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
     },
     {
         id:2,
-        title:"这是第二篇博客",
+        title:"自然语言处理技术应用",
         href:"https://www.baidu.com/",
         image:"https://images.unsplash.com/photo-1551970634-747846a548cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
     },
     {
         id:3,
-        title:"这是第三篇博客",
+        title:"敬请期待",
         href:"https://map.baidu.com/",
         image:"https://images.unsplash.com/photo-1506634064465-7dab4de896ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
     },
